7f73b9f757e76fb80818a7121d3031ed7daa4189
stevew <stevewrightnz@hotmail.com>
2018-02-01 22:12:25 +1300

    Number 12
    more

---
+++ rootsmagic/alternate/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=yes">
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="author" content="Steve Wright">
+<link rel="shortcut icon" href="favicon.ico" >
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="wrightfamily_gen.css">
+<link rel="stylesheet" href="fonts.css">
+<script src="wrightfamily.js"></script>
+<title>Wright Family Genealogy</title>
+</head>
+
+<body>
+
+	<table id='t01'>
+	<tr><td class="navbar" style='text-align:center;' >
+			
+				<a href="index.html" class="navlnk">Home</a> - 
+				<a href="surnames.html" class="navlnk">Surname List</a> - 
+				<a href="names.html" class="navlnk">Name Index</a> - 
+				<a href="sources.html" class="navlnk">Sources</a>
+			
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<h1>The Wright Family</h1>
+
+			<table id='t02'>
+			<tr>
+				<td class="NavBar"><b>Table of Contents</b></td>
+			</tr>
+			<tr>
+				<td>
+					<ul>
+						<!--Make Nicko the root person before running the website creation routines. -->
+						<li><a href="p1.html" class="lnk">Ancestors of Nicholas "Nicko" Wright</a></li>
+						<li><a href="surnames.html" class="lnk">Surnames - A list of all surnames on this site</a></li>
+						<li><a href="names.html" class="lnk">Names - A list of all names on this site</a></li>
+						<li><a href="sources.html" class="lnk">Sources - A list of all sources on this site</a></li>
+						<li><a href="../../genealogy.php" class="lnk">Genealogy Home</a></li>
+						<li><a href="/index.php" class="lnk">Home</a></li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
+				<td class="NavBar"><b><a href="../../contact.php">Contact Information</a></b></td>
+			</tr>
+			<tr>
+				<td></td>
+			</tr>
+			</table>
+			<hr> 
+			<p>Created <?php echo(filemtime(__FILE__));?> with <a href="http://www.rootsmagic.com" class="lnk">RootsMagic Genealogy Software </a></p>
+		</td>
+	</tr>
+	</table>
+</body>
+</html>
