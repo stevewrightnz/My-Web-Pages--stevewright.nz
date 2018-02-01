07933ad7305fc4647721bff0a15a6e1e65a45dfe
stevew <stevewrightnz@hotmail.com>
2018-02-01 21:12:52 +1300

    First pull request
    My first pull request to figure out how to do it

---
+++ abbreviations_gen.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=yes">
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="author" content="Steve Wright">
+<link rel="shortcut icon" href="favicon.ico" >
+<link rel="stylesheet" type="text/css" href="wrightfamily_gen.css">
+<script src="wrightfamily.js"></script>
+<title>Genealogy Abbreviations</title>
+</head>
+
+<body class="site">
+
+<?php include("includes/header.php")?>
+	
+	<main class="site-content">
+	<h3>Genealogy Abbreviations</h3>
+	<table>
+	<tr> <th>Abbreviation</th>  <th>Meaning</th></tr>
+	<tr> <td>ae/aet</td>  <td>about the age of</td></tr>	
+	<tr> <td>ob</td>  <td>deceased</td></tr>
+	<tr> <td>osp</td>  <td>(Latin, obiit sine prole) died without issue. </td></tr>	
+	<tr> <td>s.p</td>  <td>sine prole; without offspring died without issue (from Latin: decessit sine prole)</td></tr>
+	<tr> <td>uxor.</td>  <td>wife, spouse, consort</td></tr>
+
+
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>
+	<tr> <td></td>  <td></td></tr>	
+	</table>
+	
+	</main>
+<?php $pagemodified = filemtime(__FILE__);
+include("includes/footer.php");?>
+</body> 
+</html>
